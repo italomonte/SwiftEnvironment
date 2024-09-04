4cc47caf561ad12df661ad1f8477847ab7b5d0ec
Ítalo Monte <italopessoal@outlook.com>
2024-09-04 08:49:10 -0400

    update .gitignore and multipeer project

---
+++ Multipeer Connectivity/PeerApp/PeerApp/PeerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerAppApp.swift
+//  PeerApp
+//
+//  Created by Italo Guilherme Monte on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PeerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
