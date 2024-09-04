60cd1143af9f08977309ffa9a148b56acc5a1d93
Ítalo Monte <italopessoal@outlook.com>
2024-09-04 11:09:22 -0400

    ForEach, Identifiable e hashable

---
+++ ForEach, Identifiable e Hashable/ForEach, Identifiable e Hashable.swiftpm/WithAtributeAsId.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+private struct Mac {
+    let serialNumber: String
+    let capacity: Int
+    let ramMemory: Int
+    var macOSVersion: Float
+}
+
+struct WithAtributeAsId: View {
+    
+    private let MacList: [Mac] = [
+        Mac(serialNumber: "A1234", capacity: 64, ramMemory: 16, macOSVersion: 17.4),
+        Mac(serialNumber: "B5678", capacity: 128, ramMemory: 32, macOSVersion: 17.2),
+        Mac(serialNumber: "B3458", capacity: 1024, ramMemory: 32, macOSVersion: 17.2),
+        Mac(serialNumber: "D2658", capacity: 256, ramMemory: 16, macOSVersion: 17.2)
+    ]
+    
+    var body: some View {
+        VStack {
+            List{
+                ForEach(MacList, id: \.serialNumber) { mac in
+                    HStack{
+                        Image(systemName: "macbook")
+                       
+                        Text("Capacity: \(String(mac.capacity))")
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ForEach, Identifiable e Hashable/ForEach, Identifiable e Hashable.swiftpm/WithIdentifiable.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+private struct Mac: Identifiable {
+    let id = UUID()
+    let serialNumber: String
+    let capacity: Int
+    let ramMemory: Int
+    var macOSVersion: Float
+}
+
+struct WithIdentifiable: View {
+    
+    private let MacList: [Mac] = [
+        Mac(serialNumber: "A1234", capacity: 64, ramMemory: 16, macOSVersion: 17.4),
+        Mac(serialNumber: "B5678", capacity: 128, ramMemory: 32, macOSVersion: 17.2),
+        Mac(serialNumber: "B3458", capacity: 1024, ramMemory: 32, macOSVersion: 17.2),
+        Mac(serialNumber: "D2658", capacity: 256, ramMemory: 16, macOSVersion: 17.2)
+    ]
+    
+    var body: some View {
+        VStack {
+            List{
+                ForEach(MacList) { mac in
+                    HStack{
+                        Image(systemName: "macbook")
+                       
+                        Text("Capacity: \(String(mac.capacity))")
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
